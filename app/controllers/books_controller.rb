11eb015b39e31eecfd4be61c31855892df7badad
EC2 Default User <ec2-user@ip-172-31-39-61.ap-northeast-1.compute.internal>
2024-11-24 11:55:20 +0000

    行程１６までまで

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,25 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
+  end
+
+  def create
+    book = Book.new
+    book.save
+    redirect_to 
+  end
+
+  def show
+    @book = Book.find(params[:id])
   end
 
   def index
+    @books = Book.all  # Bookモデルから全てのデータを取得して@booksに代入する例
   end
+end
 
   def show
   end
 
   def edit
   end
-end

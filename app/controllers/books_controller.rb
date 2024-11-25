5af5ff81c5231d906bcd1e318ecdcad2f343d88d
EC2 Default User <ec2-user@ip-172-31-39-61.ap-northeast-1.compute.internal>
2024-11-25 11:25:05 +0000

    一応完成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,15 +1,17 @@
 class BooksController < ApplicationController
-  def new
-    @book = Book.new
-  end
+
 
   def create
-    book = Book.new(book_params)
-    
-    if book.save
-      redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+  
+    if @book.save
+      @books = Book.all  # 作成に成功した場合は全ての本を再取得して @books に設定
+       flash[:success] = "Book was successfully updated."
+      redirect_to book_path(@book.id)
     else
-      render 'new'
+      @books = Book.all  # 作成に失敗した場合も全ての本を @books に設定
+      flash.now[:error] = @book.errors.full_messages.join(", ")
+      render 'index'
     end
   end
 
@@ -19,6 +21,7 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all  # Bookモデルから全てのデータを取得して@booksに代入する例
+    @book = Book.new  # 新しい Book オブジェクトを生成して @book に代入する
   end
 
   def edit
@@ -32,14 +35,17 @@ class BooksController < ApplicationController
   def destroy
     @book = Book.find(params[:id])
     @book.destroy
-    redirect_to books_path, notice: "Book was successfully deleted."
+    flash[:success] = "Book was successfully deleted."  # 削除成功時のメッセージを設定
+    redirect_to books_path
   end
-
+  
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
       redirect_to @book
+       flash[:success] = "Book was successfully updated."
     else
+      flash.now[:error] = @book.errors.full_messages.join(", ")
       render 'edit'
     end
   end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,4 @@
 class Book < ApplicationRecord
+  validates :title, presence: true  # タイトルが空でないことを検証
+  validates :body, presence: true   # 本文が空でないことを検証
 end
